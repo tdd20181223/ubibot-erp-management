2479f737b9507fd755fd34834672f77bda1b1218
tiandandan <1259206679@qq.com>
2026-01-08 15:13:04 +0800

    feat: initialize Ant Design Pro project

---
+++ src/pages/account/account-center/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MyPage: React.FC = () => {
+  return <div>这是我的新页面</div>;
+};
+
+export default MyPage;
---
+++ src/pages/account/index.tsx
@@ -0,0 +1,16 @@
+import { PageContainer } from '@ant-design/pro-components';
+// import { useModel } from '@umijs/max';
+// import { Card, theme } from 'antd';
+import React from 'react';
+
+const Welcome: React.FC = () => {
+  //   const { token } = theme.useToken();
+  //   const { initialState } = useModel('@@initialState');
+  return (
+    <PageContainer>
+      <div>个人中心页面</div>
+    </PageContainer>
+  );
+};
+
+export default Welcome;
