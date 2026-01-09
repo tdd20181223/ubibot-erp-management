415fe51d35c806dc2c667dd67e3afaa73bbe5fcf
tiandandan <1259206679@qq.com>
2026-01-09 13:15:49 +0800

    feat: 编辑个人中心页面。

--- src/pages/account/account-center/index.tsx
+++ src/pages/account/account-center/index.tsx
@@ -1,10 +1,22 @@
 import { PageContainer } from '@ant-design/pro-components';
+import { Card, theme } from 'antd';
 import React from 'react';
 
 const AccountCenter: React.FC = () => {
   return (
-    <PageContainer>
-      <div>个人中心页面</div>
+    <PageContainer
+      header={{
+        title: '页面标题',
+        breadcrumb: {},
+      }}
+    >
+      <Card
+        style={{
+          borderRadius: 8,
+        }}
+      >
+        <div className="background-white">个人中心页面</div>
+      </Card>
     </PageContainer>
   );
 };
